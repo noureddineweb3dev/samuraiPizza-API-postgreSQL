5a2c98a182723cdbf3fc16700190e1736b455911
NOUREDDINE <noureddine.web3dev@gmail.com>
2026-01-27 03:55:00 +0100

    Import path for protect middleware in favoritesRoutes corrected

--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getFavorites, addFavorite, removeFavorite } from '../controllers/favoritesController.js';
-import { protect } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
